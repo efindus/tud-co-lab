0912a7771b41845c1dc209fbfa709a990a6974a0
efindus <48939809+efindus@users.noreply.github.com>
2024-09-10 02:22:02 +0200

    So apparently my code isn't good enough :(((
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

--- a1/a1-z.s
+++ a1/a1-z.s
@@ -1,80 +1,102 @@
+# ******************************************************************
+# Assignment 1, CSE1400                                            *
+# by: Zofia Krok, Michał Olszewski                                 *
+#                                                                  *
+# Description:                                                     *
+# This code computes the result of raising the first passed number *
+# to the power of the second passed number. Arguments are supplied *
+# through stdin.  Note: this program uses the fast power algorithm *
+# to speed-up the process.                                         *
+# ******************************************************************
+
 .data
-    input: .asciz "%ld"  # string format for scanf
-    output: .asciz "%ld"  # string format for scanf
+	input: .asciz "%ld"     # format string for scanf
+	output: .asciz "%ld\n"  # format string for printf
 
 .global main
+
 .text
 
-
 main:
-    push %rbp
-    mov %rsp, %rbp # epilogue
+	# prologue
+	push %rbp                       # save the base pointer on the stack
+	mov %rsp, %rbp                  # set up the new base pointer
 
-    sub $16, %rsp
+	# allocating memory
+	sub $16, %rsp                   # allocate 16 bytes on the stack (for two quads)
 
-    movq $0, %rax
-    movq $input, %rdi
-    leaq -16(%rbp), %rsi
-    call scanf
+	# reading the first number (base)
+	movq $0, %rax                   # required clearing for scanf
+	movq $input, %rdi               # move input format into %rdi (first argument)
+	leaq -16(%rbp), %rsi            # set the address where to read the first number
+	call scanf                      # read the first number
 
-    movq $0, %rax
-    movq $input, %rdi
-    leaq -8(%rbp), %rsi
-    call scanf
+	# reading the second number (exponent)
+	movq $0, %rax
+	movq $input, %rdi
+	leaq -8(%rbp), %rsi
+	call scanf
 
-    movq -16(%rbp), %rdi
-    movq -8(%rbp), %rsi
+	# setting the argumnets to call the function
+	movq -16(%rbp), %rdi            # load the first number into %rdi (first argument)
+	movq -8(%rbp), %rsi             # load the second number inot %rsi (second argument)
 
-    call fastpow
+	# computing the result
+	call fastpow                    # call fastpow to compute the result (base^exponent) in %rax
 
-    movq $0, %rax
-    movq $output, %rdi
-    movq %r10, %rsi
-    call printf
+	# printing the result
+	movq %rax, %rsi                 # move the result into the second argument
+	movq $0, %rax                   # required clearing for printf
+	movq $output, %rdi              # move the output format into the first argument
+	call printf                     # print the result
 
-    mov %rbp, %rsp
-    pop %rbp
+	# epilogue
+	mov %rbp, %rsp                  # restore the previous stack pointer
+	pop %rbp                        # restore the previous base pointer
 
-    mov $0, %rdi
-    call exit
+	mov $0, %rdi                    # set the exit status code
+	call exit                       # terminate the program
 
 
+# ************************************************************
+# Subroutine: fastpow                                        *
+# Description: computes base ** exponent                     *
+# Parameters:                                                *
+#   first: the value of base (as 64 bit integer)             *
+#   second: the value of exponent (as 64 bit integer)        *
+#   return: base ** exponent (base to the power of e)        *
+# ************************************************************
 fastpow:
-    push %rbp
-    mov %rsp, %rbp #copies stack pointer into base pointer
+	push %rbp                       # save the base pointer
+	mov %rsp, %rbp                  # set up the new base pointer
 
-    movq $1, %r10 # result
-    movq %rsi, %r11 # exp
+	movq $1, %r10                   # initialize the result
+	movq %rsi, %r11                 # copy the second argument (exponent) to %r11
 
-    cmp $0, %r11
-    jg powloop
-    mov %rbp, %rsp
-    pop %rbp
-    ret
-
-
+# checking if the exponent is odd
 powloop:
-    mov $0, %rdx
-    mov %r11, %rax
-    mov $2, %r8
-    idiv %r8
-    cmp $1, %rdx
-    jl rest
+	mov $0, %rdx                    # required clearing for division
+	mov %r11, %rax                  # move the exponent (dividend) into %rax
+	mov $2, %r8                     # move 2 (divisor) into %r8
+	idiv %r8                        # divide the exponent by 2 (%rax - quotient, %rdx - remainder)
+	cmp $1, %rdx                    # check if the exponent is odd (remainder is 1)
+	jl rest                         # if exponent is even jump to rest (skip multiplication of the result)
 
+	imul %rdi, %r10                 # multiply the result by the base (only if the exponent is odd)
 
-mul:
-    imul %rdi, %r10
-
-
+# continuing the exponentiation
 rest:
-    imul %rdi, %rdi
-    mov $0, %rdx
-    mov %r11, %rax
-    mov $2, %r8
-    idiv %r8
-    mov %rax, %r11
-    cmp $0, %r11
-    jg powloop
-    mov %rbp, %rsp
-    pop %rbp
-    ret
+	imul %rdi, %rdi                 # square the base
+	mov $0, %rdx                    # required clearing for division
+	mov %r11, %rax                  # move the exponent (divident) into %rax
+	mov $2, %r8                     # move 2 (divisor) into %r8
+	idiv %r8                        # divide the exponent by 2 (%rax - quotient, %rdx - remainder)
+	mov %rax, %r11                  # set the new exponent (previous one divided by 2)
+	cmp $0, %r11                    # compare the exponent with 0
+	jg powloop                      # if exponent is greater than 0, jump to powloop (continue the alghoritm)
+	mov %r10, %rax                  # otherwise, the exponentiation is complete, move the result to %rax
+
+	# epilogue
+	mov %rbp, %rsp                  # restore the previous stack pointer
+	pop %rbp                        # restore the previous base pointer
+	ret                             # return from the function
