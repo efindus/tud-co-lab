de399e1a67ef6c5ed007cb5023d9ba8e99a9cfe5
efindus <48939809+efindus@users.noreply.github.com>
2024-09-24 17:15:04 +0200

    Testing!!! test.s is the very thing that helped me generate opcodes for my brainfuck JIT
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

--- test.s
+++ test.s
@@ -6,6 +6,68 @@
     .global main
 
 main:
+//     pushq $42
+//     pushq $0xC0AF1FF
+
+//     movq %rsp, %rbp
+
+//     pushq $10
+//     pushq $123
+//     pushq $0x0F15FFFF
+//     pushq $13
+//     pushq $2
+//     pushq $666
+
+//     movq %rbp, %rsp
+//     subq $8, %rsp
+
+//     movq $0, %rsi
+//     pushq %rsi
+//     call foo
+//     popq %rdi
+//     decq %rax
+//     addq %rdi, %rax
+//     pushq %rax
+
+//     movq $0, %rdi
+//     call exit
+
+// foo:
+//     pushq %rbp
+//     movq %rsp, %rbp
+
+//     addq $42, %rsi
+//     pushq %rsi
+//     decq %rsi
+//     popq %rax
+//     pushq %rsi
+
+//     movq %rbp, %rsp
+//     popq %rbp
+//     ret
+
+
+    movq $1, %rax
+    pushq $8
+    pushq $4
+    pushq $7
+    movq $5, %rdi
+    addq $8, %rsp
+    call foo
+    pushq $8
+
+foo:
+    pushq %rbp
+    movq %rsp, %rbp
+
+    pushq %rdi
+    mulq -8(%rbp)
+    addq 16(%rbp), %rax
+
+    movq %rbp, %rsp
+    popq %rbp
+    ret
+
     // # Open the file (sys_open)
     // mov $2, %rax                          # sys_open system call number
     // lea filename(%rip), %rdi               # Filename (first argument)
